374b7cfe5ee11348ee1064541917fe5010a616e0
annoo <ann.peeters.email@gmail.com>
2017-06-09 10:36:41 +0200

    gitTest Qt

--- src/ball.h
+++ src/ball.h
@@ -1,3 +1,5 @@
+//gittest
+
 #ifndef _BALL // if this class hasn't been defined, the program can define it
 #define _BALL // by using this if statement you prevent the class to be called more than once which would confuse the compiler
 #include "ofMain.h" // we need to include this to have a reference to the openFrameworks framework
